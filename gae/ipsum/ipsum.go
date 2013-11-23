c6687646aae6d003431bc515d1ba62b50f7acec9
Thomas Rampelberg <thomas@saunter.org>
2013-11-23 15:32:23 -0800

    Switched over to using purely static files, much faster

--- gae/ipsum/ipsum.go
+++ gae/ipsum/ipsum.go
@@ -1,13 +1,61 @@
 package ipsum
 
 import(
+  "io/ioutil"
   "log"
+  "encoding/json"
+  "fmt"
+  "math/rand"
   "net/http"
+  "strconv"
 
   "github.com/ant0ine/go-json-rest"
 )
 
+type Book struct {
+  Author string
+  Title string
+  Id int
+  Paragraphs int
+  Data []Paragraph
+}
+
+type Paragraph struct {
+  Text string
+  Characters int
+  Sentences int
+}
+
+type Response struct {
+  Author string
+  Title string
+  Data []string
+}
+
 func getParagraphs(w *rest.ResponseWriter, r *rest.Request) {
+  count, _ := strconv.Atoi(r.FormValue("num"))
+  if count == 0 { count = 4 }
+
+  available_books, _ := ioutil.ReadDir("books")
+
+  book_name := available_books[rand.Intn(len(available_books))].Name()
+  raw_book, _ := ioutil.ReadFile(fmt.Sprintf("books/%s", book_name))
+
+  var book Book
+  var resp Response
+  json.Unmarshal(raw_book, &book)
+  start := rand.Intn(book.Paragraphs - count)
+
+  var subset []string
+  for _, v := range book.Data[start:start+count] {
+    subset = append(subset, v.Text)
+  }
+
+  resp.Author = book.Author
+  resp.Title = book.Title
+  resp.Data = subset
+
+  w.WriteJson(resp)
 }
 
 func init() {
