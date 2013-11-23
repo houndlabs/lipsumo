629990e42210e8e24a48f4db3a9131f97a4e2a2d
Thomas Rampelberg <thomas@saunter.org>
2013-11-22 16:39:39 -0800

    Now able to upload books and fetch random paragraphs

--- convert.py
+++ convert.py
@@ -1,9 +1,9 @@
+import copy
 import json
-import os
 import re
 import sys
-import urllib
-import urllib2
+
+import requests
 
 raw_books = [(x, open(os.path.join('books', x), 'r').read()) for x in os.listdir('./books')]
 converted_books = []
@@ -15,8 +15,6 @@ metadata = {
   'author': re.compile('Author:\s?(.*)$', re.M)
 }
 
-url = "http://localhost:8080/paragraphs"
-
 for fname, book in raw_books:
   data = {}
   for n, r in metadata.iteritems():
@@ -25,14 +23,21 @@ for fname, book in raw_books:
       data[n] = result.group(1).strip()
 
   content = footer.split(header.split(book)[1])[0]
-  data['paragraphs'] = [x.strip() for x in content.split('\r\n\r\n') if len(x) > 0 and len(x.split('.')) > 1]
+  paragraphs = [x.strip() for x in content.strip().split('\r\n\r\n') if len(x) > 0]
   data['id'] = int(fname[2:-4])
-  print '%s:\t%s' % (data['id'], len(data['paragraphs']))
+  data['paragraphs'] = len(paragraphs)
+  blob = copy.copy(data)
+  blob['data'] = paragraphs
 
-  req = urllib2.Request(url)
-  req.add_header('Content-Type', 'application/json')
+  print '%s:' % (data['id'], data['paragraphs'])
 
   try:
-    resp = urllib2.urlopen(req, json.dumps(data))
+    r = requests.get("http://localhost:8080/upload")
+    files = { 'file': json.dumps(blob) }
+    r = requests.post(json.loads(r.text)['url'], files=files)
+
+    data['key'] = json.loads(r.text)['key']
+    r = requests.post("http://localhost:8080/books", json.dumps(data),
+      headers={'Content-type': 'application/json'})
   except Exception, e:
     print e.read()
--- gae/ipsum/ipsum.go
+++ gae/ipsum/ipsum.go
@@ -1,22 +1,74 @@
 package hello
 
 import (
-  "appengine"
-  "appengine/datastore"
-  "github.com/ant0ine/go-json-rest"
+  "io"
+  "io/ioutil"
+  "log"
+  "encoding/json"
+  "fmt"
+  "math/rand"
   "net/http"
   "strconv"
+
+  "appengine"
+  "appengine/blobstore"
+  "appengine/datastore"
+//  "appengine/memcache"
+  "appengine/urlfetch"
+
+  "github.com/ant0ine/go-json-rest"
 )
 
 type Book struct {
   Author string
   Title string
   Id int
-  Paragraphs []string `datastore:",noindex"`
+  Paragraphs int
+  Key string
+}
+
+type BookBlob struct {
+  Book
+
+  Data []string
 }
 
 func GetParagraphs(w *rest.ResponseWriter, r *rest.Request) {
-  w.WriteJson(map[string]string{"a": "b"})
+  count, _ := strconv.Atoi(r.FormValue("num"))
+  if count == 0 { count = 4 }
+
+  c := appengine.NewContext(r.Request)
+  q := datastore.NewQuery("Book")
+
+  var books []Book
+  q.GetAll(c, &books)
+
+  book := books[rand.Intn(len(books))]
+  start := rand.Intn(book.Paragraphs - count)
+
+  // ------
+  client := urlfetch.Client(c)
+  resp, err := client.Get(fmt.Sprintf("http://localhost:8080/books/%d", book.Id))
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  defer resp.Body.Close()
+  body, _ := ioutil.ReadAll(resp.Body)
+
+  var blob BookBlob
+  json.Unmarshal(body, &blob)
+  blob.Data = blob.Data[start:start+count]
+  w.WriteJson(blob)
+}
+
+func GetBook(w *rest.ResponseWriter, r *rest.Request) {
+  c := appengine.NewContext(r.Request)
+  key := datastore.NewKey(c, "Book", r.PathParam("id"), 0, nil)
+  var book Book
+  datastore.Get(c, key, &book)
+  blobstore.Send(w.ResponseWriter, appengine.BlobKey(book.Key))
 }
 
 func StoreBook(w *rest.ResponseWriter, r *rest.Request) {
@@ -28,16 +80,36 @@ func StoreBook(w *rest.ResponseWriter, r *rest.Request) {
     rest.Error(w, err.Error(), http.StatusInternalServerError)
     return
   }
-
-
   w.WriteJson(map[string]string{ "success": "true" })
+}
+
+func UploadID(w *rest.ResponseWriter, r *rest.Request) {
+  c := appengine.NewContext(r.Request)
+  uploadURL, err := blobstore.UploadURL(c, "/upload/id", nil)
+  if err != nil {
+    rest.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.WriteJson(map[string]string{ "succes": "true", "url": uploadURL.String() })
+}
+
+func BlobID(w http.ResponseWriter, r *http.Request) {
+  blobs, _, _ := blobstore.ParseUpload(r)
+  io.WriteString(w, fmt.Sprintf("{ \"success\":\"true\", \"key\":\"%s\" }",
+    blobs["file"][0].BlobKey))
 }
 
 func init() {
   handler := rest.ResourceHandler{}
   handler.SetRoutes(
     rest.Route{"GET", "/paragraphs", GetParagraphs},
-    rest.Route{"POST", "/paragraphs", StoreBook},
+    rest.Route{"GET", "/books/:id", GetBook},
+    rest.Route{"POST", "/books", StoreBook},
+    rest.Route{"GET", "/upload", UploadID},
   )
   http.Handle("/", &handler)
+  http.HandleFunc("/upload/id", BlobID)
+
+  log.Print("start")
 }
