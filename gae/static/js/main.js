fdcf521226b5b7ef0078935baaa038423f78be26
Thomas Rampelberg <thomas@saunter.org>
2013-11-26 15:53:59 -0800

    get rid of the dropdown

--- gae/static/js/main.js
+++ gae/static/js/main.js
@@ -9,19 +9,24 @@
           return $($(this).data('contentwrapper')).html();
       }
   }).on('shown.bs.popover', function() {
+    var self = this;
     $(".num-paragraphs .amount a").click(function() {
-      redraw($(this).attr('num'));
+      redraw($(this).attr('num'), function() {
+        $(self).popover('hide');
+      });
     });
   });
 
   // -- Page refresh
 
-  var redraw = function(num) {
+  var redraw = function(num, cb) {
     if (!num) { num = 4 }
     $.get("/paragraphs?num=" + num, function(resp) {
       $(".canvas").html(_.map(resp.Data, function(p) {
         return "<p>" + p + "</p>"
       }));
+
+      if (cb) { cb() }
     });
   };
 
